d36607acdd521ccda8f4d9fcc76d547fd3f5baf7
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-09-26 13:43:38 -0400

    definition display tweaking

--- src/Results.js
+++ src/Results.js
@@ -2,17 +2,14 @@ import React from "react";
 import "./Results.css";
 
 export default function Results(props){
-console.log(props.results.meanings);
+console.log(props.results.meanings[0].definition);
 
 if (props.results){
-    return <div className="Results">
-
+    return (<div className="Results">
       <h2>{props.results.word}</h2>
-            <div>
-             {props.results.meanings[0].definition}
-                </div>
-
-    </div>;
+           <p>
+                </p>
+    </div>)
 }else{
     return null;
 }
