1c8b66fdb71300262fe8d167facf8535aa446dd6
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-10-02 14:41:32 -0400

    added default word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,10 @@ import "./Dictionary.css"
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary(){
-    let [keyWord, setKeyword] = useState("");
-    let [results, setResults] = useState(null);    
+export default function Dictionary(props){
+    let [keyWord, setKeyword] = useState(props.defaultKeyword);
+    let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);    
 
 
 function handleResponse(response){
@@ -14,8 +15,7 @@ function handleResponse(response){
 }
 
 
-function search(event){
-    event.preventDefault();
+function search(){
     let apiKey = `01dd2bca25c0t00b3d253f443e0of791`
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`
     axios.get(apiUrl).then(handleResponse);
@@ -26,12 +26,25 @@ function handleKeyWordChange(event){
     setKeyword(event.target.value);
     }
 
+    function handleSubmit(event){
+        event.preventDefault();
+        search();
+    }
+
+    function load(){
+        setLoaded(true)
+        search();
+    }
+if(loaded){
     return <div className="Dictionary">
         <div>
-            <form onSubmit={search}>
+            <form onSubmit={handleSubmit}>
                 <input type="search" autoFocus={true} onChange={handleKeyWordChange}/>
             </form>
         </div>
             <Results results={results} />
     </div>;
-}
+} else {
+    load();
+    return "loading"
+}}
